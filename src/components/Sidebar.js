9a92dee1232516a2db784f50eaa5ed4a4aa236e1
Manikantatechis <manikanta@techis.io>
2023-01-25 05:11:35 +0530

    updated loading and css

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -6,7 +6,7 @@ import * as AiIcons from 'react-icons/ai';
 import { SidebarData } from './SidebarData';
 import SubMenu from './SubMenu';
 import { IconContext } from 'react-icons/lib';
-import Logo from '../assets/img/logo.svg'
+import Logo from '../assets/img/logo1.svg'
 
 const Nav = styled.div`
   background-image: linear-gradient(45deg,#006,#001d7a 11%,#00348f 22%,#00479e 33%,#005aa8 44%,#006eb3 56%,#0081bd 67%,#0095c2 78%,#00a6c7 89%,#16b8ca);
--- src/router.jsx
+++ src/router.jsx
@@ -13,13 +13,14 @@ export default function Router() {
     const [sidebar, setSidebar] = useState(false);
     const showSidebar = () => setSidebar(!sidebar);
     const showSidebar1 = () => setSidebar(false);
+    const [loading, setLoading] = useState(true)
 
     return (
         <>
             <Sidebar sidebar={sidebar} showSidebar={showSidebar} />
             <Routes>
                 <Route path='/' element={<Home showSidebar1={showSidebar1} />} />
-                <Route path='/step1/:fileName' element={<Notebook showSidebar1={showSidebar1} />} />
+                <Route path='/step1/:fileName' element={<Notebook showSidebar1={showSidebar1} setLoading={setLoading} loading={loading} />} />
             </Routes>
         </>
 
